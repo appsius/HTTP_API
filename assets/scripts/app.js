fdd44adda3747f2fa91cea9fc4b2c031e8724d72
appsius <ilyas.ustunkaya234434@gmail.com>
2020-05-16 15:10:47 +0300

    Axios Lib && Http Request

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,56 +4,54 @@ const form = document.querySelector('#new-post form');
 const fetchButton = document.querySelector('#available-posts button');
 const postList = document.querySelector('ul');
 
-function sendHttpRequest(method, url, data) {
-	// const promise = new Promise((resolve, reject) => {
-	// const xhr = new XMLHttpRequest();
-	// xhr.open(method, url);
-	// xhr.responseType = 'json';
-	// xhr.onload = function () {
-	// 	if (xhr.status >= 200 && xhr.status < 300) {
-	// 		resolve(xhr.response);
-	// 	} else {
-	// 		reject(new Error('Unable to fetch posts!'));
-	// 	}
-	// };
-	// xhr.onerror = function () {
-	// 	reject(new Error('Failed to send request!'));
-	// };
-	// xhr.send(JSON.stringify(data));
+// function sendHttpRequest(method, url, data) {
+// 	// const promise = new Promise((resolve, reject) => {
+// 	// const xhr = new XMLHttpRequest();
+// 	// xhr.open(method, url);
+// 	// xhr.responseType = 'json';
+// 	// xhr.onload = function () {
+// 	// 	if (xhr.status >= 200 && xhr.status < 300) {
+// 	// 		resolve(xhr.response);
+// 	// 	} else {
+// 	// 		reject(new Error('Unable to fetch posts!'));
+// 	// 	}
+// 	// };
+// 	// xhr.onerror = function () {
+// 	// 	reject(new Error('Failed to send request!'));
+// 	// };
+// 	// xhr.send(JSON.stringify(data));
 
-	// });
-	return fetch(url, {
-		method: method,
-		// body: JSON.stringify(data),
-		body: data,
-		// headers: {
-		// 	'Content-Type': 'application/json',
-		// },
-	})
-		.then((response) => {
-			if (response.status >= 200 && response.status < 300) {
-				return response.json();
-			} else {
-				return response.json().then((errorData) => {
-					console.log(errorData);
-					throw new Error('Something went wrong - server side!');
-				});
-			}
-		})
-		.catch((error) => {
-			console.log(error);
-			throw new Error('Something went wrong!');
-		});
-}
+// 	// });
+// 	return fetch(url, {
+// 		method: method,
+// 		// body: JSON.stringify(data),
+// 		body: data,
+// 		// headers: {
+// 		// 	'Content-Type': 'application/json',
+// 		// },
+// 	})
+// 		.then((response) => {
+// 			if (response.status >= 200 && response.status < 300) {
+// 				return response.json();
+// 			} else {
+// 				return response.json().then((errorData) => {
+// 					console.log(errorData);
+// 					throw new Error('Something went wrong - server side!');
+// 				});
+// 			}
+// 		})
+// 		.catch((error) => {
+// 			console.log(error);
+// 			throw new Error('Something went wrong!');
+// 		});
+// }
 
 async function fetchPost() {
 	try {
-		const responseData = await sendHttpRequest(
-			'GET',
+		const response = await axios.get(
 			'https://jsonplaceholder.typicode.com/posts'
 		);
-
-		const listOfPosts = responseData;
+		const listOfPosts = response.data;
 		for (let post of listOfPosts) {
 			const postEl = document.importNode(postTemplate.content, true);
 			postEl.querySelector('h2').textContent = post.title;
@@ -63,6 +61,7 @@ async function fetchPost() {
 		}
 	} catch (error) {
 		alert(error.message);
+		console.log(error.response);
 	}
 }
 
@@ -79,7 +78,11 @@ async function createPost(title, content) {
 	// fd.append('body', content);
 	fd.append('userId', userId);
 
-	sendHttpRequest('POST', 'https://jsonplaceholder.typicode.com/posts', fd);
+	const response = await axios.post(
+		'https://jsonplaceholder.typicode.com/posts',
+		fd
+	);
+	console.log(response);
 }
 
 fetchButton.addEventListener('click', fetchPost());
@@ -94,9 +97,6 @@ form.addEventListener('submit', (event) => {
 postList.addEventListener('click', (event) => {
 	if (event.target.tagName === 'BUTTON') {
 		const postId = event.target.closest('li').id;
-		sendHttpRequest(
-			'DELETE',
-			`https://jsonplaceholder.typicode.com/posts/${postId}`
-		);
+		axios.delete(`https://jsonplaceholder.typicode.com/posts/${postId}`);
 	}
 });
