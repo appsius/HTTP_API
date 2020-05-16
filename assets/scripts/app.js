a9f0ade1a8dc6a743ad4f2080824af016915ecda
appsius <ilyas.ustunkaya234434@gmail.com>
2020-05-16 13:22:19 +0300

    Working with FormData

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -24,10 +24,11 @@ function sendHttpRequest(method, url, data) {
 	// });
 	return fetch(url, {
 		method: method,
-		body: JSON.stringify(data),
-		headers: {
-			'Content-Type': 'application/json',
-		},
+		// body: JSON.stringify(data),
+		body: data,
+		// headers: {
+		// 	'Content-Type': 'application/json',
+		// },
 	})
 		.then((response) => {
 			if (response.status >= 200 && response.status < 300) {
@@ -73,7 +74,12 @@ async function createPost(title, content) {
 		userId: userId,
 	};
 
-	sendHttpRequest('POST', 'https://jsonplaceholder.typicode.com/posts', post);
+	const fd = new FormData(form);
+	// fd.append('title', title);
+	// fd.append('body', content);
+	fd.append('userId', userId);
+
+	sendHttpRequest('POST', 'https://jsonplaceholder.typicode.com/posts', fd);
 }
 
 fetchButton.addEventListener('click', fetchPost());
