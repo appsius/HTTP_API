a9b1a81396a48593096194135443aca8cd3fb380
appsius <ilyas.ustunkaya234434@gmail.com>
2020-05-16 12:34:49 +0300

    Using Fetch API

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,47 +5,46 @@ const fetchButton = document.querySelector('#available-posts button');
 const postList = document.querySelector('ul');
 
 function sendHttpRequest(method, url, data) {
-	const promise = new Promise((resolve, reject) => {
-		const xhr = new XMLHttpRequest();
+	// const promise = new Promise((resolve, reject) => {
+	// const xhr = new XMLHttpRequest();
+	// xhr.open(method, url);
+	// xhr.responseType = 'json';
+	// xhr.onload = function () {
+	// 	if (xhr.status >= 200 && xhr.status < 300) {
+	// 		resolve(xhr.response);
+	// 	} else {
+	// 		reject(new Error('Unable to fetch posts!'));
+	// 	}
+	// };
+	// xhr.onerror = function () {
+	// 	reject(new Error('Failed to send request!'));
+	// };
+	// xhr.send(JSON.stringify(data));
 
-		xhr.open(method, url);
-		xhr.responseType = 'json';
-
-		xhr.onload = function () {
-			if (xhr.status >= 200 && xhr.status < 300) {
-				resolve(xhr.response);
-			} else {
-				reject(new Error('Unable to fetch posts!'));
-			}
-		};
-
-		xhr.onerror = function () {
-			reject(new Error('Failed to send request!'));
-		};
-
-		xhr.send(JSON.stringify(data));
+	// });
+	return fetch(url).then((response) => {
+		return response.json();
 	});
-	return promise;
 }
 
 async function fetchPost() {
-	try {
-		const responseData = await sendHttpRequest(
-			'GET',
-			'https://jsonplaceholder.typicode.com/po'
-		);
+	// try {
+	const responseData = await sendHttpRequest(
+		'GET',
+		'https://jsonplaceholder.typicode.com/posts'
+	);
 
-		const listOfPosts = responseData;
-		for (let post of listOfPosts) {
-			const postEl = document.importNode(postTemplate.content, true);
-			postEl.querySelector('h2').textContent = post.title;
-			postEl.querySelector('p').textContent = post.body;
-			postEl.querySelector('li').id = post.id;
-			listElement.append(postEl);
-		}
-	} catch (error) {
-		alert(error.message);
+	const listOfPosts = responseData;
+	for (let post of listOfPosts) {
+		const postEl = document.importNode(postTemplate.content, true);
+		postEl.querySelector('h2').textContent = post.title;
+		postEl.querySelector('p').textContent = post.body;
+		postEl.querySelector('li').id = post.id;
+		listElement.append(postEl);
 	}
+	// } catch (error) {
+	// 	alert(error.message);
+	// }
 }
 
 async function createPost(title, content) {
